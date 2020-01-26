f94676b88a5ee0ee776d8672d2f79d39ddc56711
AlexApps99 <alex.apps99@gmail.com>
2020-01-27 12:34:20 +1300

    Added more r e v e n u e s t r e a m s

--- sparkmemes.py
+++ sparkmemes.py
@@ -415,8 +415,8 @@ if __name__ == "__main__":
   render(posts, imgs, True)
   yt = authenticate()
   video_id = upload(yt, args.name.replace('{}',str((datetime.now(timezone.utc)-(datetime(2020,1,1,tzinfo=timezone.utc)+timedelta(days=args.offset))).days)), args.description, args.tags)
-  upload_thumbnail(yt, gen_thumbnail(imgs[0]), video_id)
-  upload_captions(yt, video_id, "authors")
+  #upload_thumbnail(yt, gen_thumbnail(imgs[0]), video_id)
+  #upload_captions(yt, video_id, "authors")
   #upload_captions(yt, video_id, "titles")
   like_video(yt, video_id)
   
