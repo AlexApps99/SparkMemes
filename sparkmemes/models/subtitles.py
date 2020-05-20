ad941f9b21901e7b75b196032dd27d78aa5ed833
AlexApps99 <alex.apps99@gmail.com>
2020-05-21 09:15:24 +1200

    Fix Subtitles on Windows (still doesn't work)

--- sparkmemes/models/subtitles.py
+++ sparkmemes/models/subtitles.py
@@ -33,5 +33,5 @@ class Subtitles:
 
     # Saves subtitles to area on disk
     def save(self, path):
-        with open(path, "w") as f:
+        with open(path, "w", encoding="utf-8") as f:
             self._save(f)
